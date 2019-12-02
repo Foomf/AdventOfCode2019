5b322e5ee08c1722134b542a8327ffcfecaf330d
Foomf <foomf@protonmail.com>
2019-12-02 01:23:43 -0800

    Puzzle 1 complete

---
+++ Day 1/Puzzle 1/AdventOfCode.Day1.Puzzle1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Day1.Puzzle1
+{
+    class Program
+    {
+        private const string InputName = "input.txt";
+
+        static async Task Main(string[] args)
+        {
+            var numbers = await ReadInputAsync();
+            var totalFuel = numbers
+                .Select(GetRequiredFuel)
+                .Aggregate(0, (total, reqFuel) => total + reqFuel);
+            Console.WriteLine($"The total fuel needed is {totalFuel}");
+        }
+
+        public static int GetRequiredFuel(int mass)
+        {
+            var val = mass / 3.0D;
+            val = Math.Floor(val);
+            val -= 2;
+            return (int) val;
+        }
+
+        public static async Task<List<int>> ReadInputAsync()
+        {
+            var lines = await File.ReadAllLinesAsync(InputName);
+            return lines.Select(int.Parse).ToList();
+        }
+    }
+}
